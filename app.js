b9040139a14fe353b99f455dbe53380e5ba8a22e
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-04-21 14:43:46 -0300

    FEAT - Cadastro de usuário e salvamento de tweet

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("hello world");
+let users = [];
+let tweets = [];
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/sign-up", (req, res)=>{
+    const user = req.body;
+    users.push(user);
+
+    res.statusMessage = "OK"
+    res.status(200).end();
 });
+
+app.post("/tweets", (req, res)=>{
+    const {username, tweet} = req.body;
+    let user = users.find((element)=>username === element.username)
+    let avatar = user.avatar
+
+    const dataTweet = {
+        username,
+        avatar,
+        tweet,
+    }
+
+    tweets.push(dataTweet);
+
+    res.statusMessage = "OK"
+    res.status(200).end();
+});
+
+app.get("/tweets", (req, res)=>{
+    // let stop = 0;
+    // let newTweets = []
+    // for (let i = tweets.length; i >= 0; i--) {
+    //     if (stop >=10) {
+    //         return;
+    //     }else{
+    //         newTweets.push(tweets[i]);
+    //     }
+    //     stop++; 
+    // }
+    res.send(tweets);
+    // console.log(newTweets);
+})
 
 app.listen(5001, ()=>{
     console.log("Oia o bixão ligado na porta 5001")
